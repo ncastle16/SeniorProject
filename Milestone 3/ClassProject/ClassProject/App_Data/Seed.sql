9f87b8fcb451d3c370fb09a9d69be82bbe49e7b5
ncastle16 <ncastle16@wou.edu>
2020-01-25 14:27:11 -0800

    Database scripts. Up,Down, Seed

---
+++ Milestone 3/ClassProject/ClassProject/App_Data/Seed.sql
@@ -0,0 +1,28 @@
+﻿INSERT INTO [dbo].[Athletes] (Name, DOB) VALUES
+	('Testy McTesty', '2000-11-11'),
+	('Testy McTesty The Second', '2000-2-3')
+
+INSERT INTO [dbo].[EventTypes] (EventName) VALUES
+	('Kendama Jam Sesh')
+
+INSERT INTO [dbo].[Locations] (LocationName) VALUES
+	('Creepy Warehouse')
+
+INSERT INTO [dbo].[Events] (LocationID, EventTypeID, EventDate) VALUES
+	('1', '1', '2020-1-25')
+
+INSERT INTO [dbo].[Coaches] (Name) VALUES 
+	('Maya tiger')
+
+INSERT INTO [dbo].[Teams] (Name, CoachID) VALUES
+	('Sweets Kendama', '1')
+
+INSERT INTO [dbo].[Times] (AthleteID, EventID, Time) VALUES
+	('1', '1', '0:0:1')
+
+INSERT INTO [dbo].[AthleteTeams] (AthleteID, TeamID) VALUES 
+	('1', '1')
+
+INSERT INTO [dbo].[EventTeams] (TeamID, EventID) VALUES 
+	('1', '1')
+GO
