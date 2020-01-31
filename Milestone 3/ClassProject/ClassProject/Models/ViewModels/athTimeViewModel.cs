830ca66defbd98ee493e0d023b3062c143f5bb29
Brad Christensen <bradchristensen755@gmail.com>
2020-01-31 10:28:31 -0800

    Adding so i dont lose them

---
+++ Milestone 3/ClassProject/ClassProject/Models/ViewModels/athTimeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClassProject.Models.ViewModels
+{
+    public class athTimeViewModel
+    {
+       
+        public virtual ICollection<Time> Times { get; set; }
+    }
+}
