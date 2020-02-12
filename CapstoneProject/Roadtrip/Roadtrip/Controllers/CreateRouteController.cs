c577a45d6829d4da2c25e17aab8740d378025ba0
Brad Christensen <bradchristensen755@gmail.com>
2020-02-12 15:12:38 -0800

    some message

---
+++ CapstoneProject/Roadtrip/Roadtrip/Controllers/CreateRouteController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Google.Apis.Plus.v1.Data;
+using LinqToTwitter;
+using Newtonsoft.Json;
+using Roadtrip.Models.ViewModels; 
+
+namespace Roadtrip.Controllers
+{
+    public class CreateRouteController : Controller
+    {
+        // GET: CreateRoute
+        
+        public ActionResult Route()
+        {
+             
+            return View();
+        }
+       /* public ActionResult Route(string name)
+        {
+            ViewBag.name = name;
+            string[] words = name.Split(' '); 
+
+            return View(); 
+        }*/
+
+        public ActionResult DisplayInfo()
+        {
+            string request = Request.QueryString["Info"];
+
+            MapInfoViewModel myInfo = new MapInfoViewModel()
+            {
+                Place = request
+            };
+             
+            
+
+            return new ContentResult
+            {
+                Content = JsonConvert.SerializeObject(myInfo),
+                ContentType = "application/json",
+                ContentEncoding = System.Text.Encoding.UTF8
+            };
+        }
+    }
+
+    
+}
