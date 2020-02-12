85d307bbd77ab87fb229fd6778460bc0712abea7
Brad Christensen <bradchristensen755@gmail.com>
2020-02-12 15:12:38 -0800

    some message

---
+++ CapstoneProject/Roadtrip/Roadtrip/Models/ViewModels/MapInfoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Roadtrip.Models.ViewModels
+{
+    public class MapInfoViewModel
+    {
+       public String Place { get; set; }
+       public String City { get; set; }
+       public String State { get; set; }
+    }
+}
---
+++ CapstoneProject/Roadtrip/Roadtrip/Scripts/main.js
@@ -0,0 +1,23 @@
+﻿function getInfo(Info) {
+    $.ajax({
+        type: "GET", 
+        dataType: "json", 
+        url: "/CreateRoute/DisplayInfo",
+        data: { 'Info': Info }, 
+        success: showInfo, 
+        error: errorOnAjax
+    })
+}
+function errorOnAjax() {
+    console.log("ERROR in ajax request.");
+}
+
+function showInfo(data) {
+    $('#outputTable').append($('<table id=\"output\">'));
+    $('#output').append($('<tr id=\"tableTr\">'));
+    $('#tableTr').append($('<th><strong><center> Times </th>'));
+    $('#tableTr').append($('<th><strong><center> Race Types </th>'));
+    $('#output').append($('</tr>'));
+    $('#outputTable').append($('</table>'));
+
+}
