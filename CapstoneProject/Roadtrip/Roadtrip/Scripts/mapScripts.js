a90fedb6adfcdb45b91a4daf9f4ac20ed3112ae5
mcaldwell16 <mcaldwell16@wou.edu>
2020-02-19 14:41:22 -0800

    Added route map.

---
+++ CapstoneProject/Roadtrip/Roadtrip/Scripts/mapScripts.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() {
+    plotMap();
+});
+
+function plotMap() {
+    var mymap = L.map('mapid').setView([47.622370, -122.347020], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
+    var greenIcon = L.icon({
+        iconUrl: 'https://cdn4.iconfinder.com/data/icons/iconsimple-places/512/pin_1-512.png',
+        iconSize: [40, 40], // size of the icon
+        shadowSize: [20, 20], // size of the shadow
+        iconAnchor: [20, 40], // point of the icon which will correspond to marker's location
+        shadowAnchor: [20, 40],  // the same for the shadow
+        popupAnchor: [0, -40] // point from which the popup should open relative to the iconAnchor
+    });
+
+    L.marker([47.622370, -122.347020], { icon: greenIcon }).addTo(mymap).bindPopup("Seattle");
+}
