c10efb13680680a73c28975a5e26f10a9239809d
mcaldwell16 <mcaldwell16@wou.edu>
2020-02-20 09:02:23 -0800

    Added routing js

--- CapstoneProject/Roadtrip/Roadtrip/Scripts/mapScripts.js
+++ CapstoneProject/Roadtrip/Roadtrip/Scripts/mapScripts.js
@@ -3,18 +3,18 @@
 });
 
 function plotMap() {
-    var mymap = L.map('mapid').setView([47.622370, -122.347020], 13);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mymap);
-    var greenIcon = L.icon({
-        iconUrl: 'https://cdn4.iconfinder.com/data/icons/iconsimple-places/512/pin_1-512.png',
-        iconSize: [40, 40], // size of the icon
-        shadowSize: [20, 20], // size of the shadow
-        iconAnchor: [20, 40], // point of the icon which will correspond to marker's location
-        shadowAnchor: [20, 40],  // the same for the shadow
-        popupAnchor: [0, -40] // point from which the popup should open relative to the iconAnchor
-    });
+    var map = L.map('mapid');
 
-    L.marker([47.622370, -122.347020], { icon: greenIcon }).addTo(mymap).bindPopup("Seattle");
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}{r}.png', {
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    L.Routing.control({
+        waypoints: [
+            L.latLng(45.309640, -122.767030),
+            L.latLng(45.517560, -122.537820)
+        ],
+        units: 'imperial',
+        routeWhileDragging: true
+    }).addTo(map);
 }
