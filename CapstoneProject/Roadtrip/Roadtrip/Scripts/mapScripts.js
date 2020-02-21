3d486517ff0c96b7b2d34badfdd91dd3dee556f8
mcaldwell16 <mcaldwell16@wou.edu>
2020-02-20 16:07:19 -0800

    Fixed and cleaned merged code.

--- CapstoneProject/Roadtrip/Roadtrip/Scripts/mapScripts.js
+++ CapstoneProject/Roadtrip/Roadtrip/Scripts/mapScripts.js
@@ -3,18 +3,17 @@
 });
 
 function plotMap() {
-    var map = L.map('mapid');
+    var map = L.map('routemap').setView([57.74, 11.94], 13);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}{r}.png', {
         attribution: '© OpenStreetMap contributors'
     }).addTo(map);
-
     L.Routing.control({
         waypoints: [
-            L.latLng(45.309640, -122.767030),
-            L.latLng(45.517560, -122.537820)
+            L.latLng(57.74, 11.94),
+            L.latLng(57.6792, 11.949)
         ],
-        units: 'imperial',
-        routeWhileDragging: true
+        router: L.Routing.mapbox('pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw')
     }).addTo(map);
+
 }
